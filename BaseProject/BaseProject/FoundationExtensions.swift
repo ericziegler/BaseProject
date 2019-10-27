3f2cb231ef330afce27d517ae42554904fa73102
Eric <eric@zigabytes.com>
2019-10-27 07:36:47 -0400

    EZ: Add convenience methods for substringing.

--- BaseProject/BaseProject/FoundationExtensions.swift
+++ BaseProject/BaseProject/FoundationExtensions.swift
@@ -20,6 +20,24 @@ extension NSRange {
 
 }
 
+// MARK: - String
+
+extension String {
+
+    subscript (bounds: CountableClosedRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start...end])
+    }
+
+    subscript (bounds: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start..<end])
+    }
+
+}
+
 // MARK: - NSDate
 
 extension Date {
