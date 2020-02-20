d2bad7e262d1fb66a91d15eb20580bc128f888fa
Eric <eric@zigabytes.com>
2020-02-20 17:35:14 -0500

    EZ: Allow tab controller to pop to root of child nav controller.

--- BaseProject/BaseProject/BaseViewControllers/BaseTabBarController.swift
+++ BaseProject/BaseProject/BaseViewControllers/BaseTabBarController.swift
@@ -6,8 +6,27 @@ import UIKit
 
 class BaseTabBarController: UITabBarController {
 
+    var curTabIndex = 0
+
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        if let tabItems = tabBar.items {
+            for i in 0..<tabItems.count {
+                if item == tabItems[i] {
+                    if curTabIndex == i {
+                        // user tapped tab they were already on. pop to root view controller
+                        if let controllers = viewControllers, let navController = controllers[i] as? BaseNavigationController {
+                            navController.popToRootViewController(animated: true)
+                        }
+                    }
+                    curTabIndex = i
+                    break
+                }
+            }
+        }
     }
 
 }
